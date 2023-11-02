0cb789213994020af7e7afc0f587cac8e34bf462
Thomas Egense <teg@kb.dk>
2023-11-02 12:42:31 +0100

    creating branch

---
+++ src/test/java/dk/kb/cdx/workflow/CdxIndexerWorkflowTest.java
@@ -0,0 +1,25 @@
+package dk.kb.cdx.workflow;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.IOException;
+
+import javax.ws.rs.core.StreamingOutput;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+
+public class CdxIndexerWorkflowTest {
+    
+    
+
+    @Test
+    void todo() throws IOException {
+                
+    }
+
+
+
+
+}
